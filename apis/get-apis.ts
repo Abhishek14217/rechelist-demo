bf7b53a776e8d4f7e0ebf91f2608bf6fa4c1830e
Abhishek14217 <abhimahajan.webhopers@gmail.com>
2025-08-23 17:05:35 +0530

    Caching issue fixed to 10sec

--- apis/get-apis.ts
+++ apis/get-apis.ts
@@ -47,107 +47,127 @@ export const fetchFromAPI = async (
 export const getCategories = () =>
   fetchFromAPI("/product-categories", {
     // next: { revalidate: 600 },
+    next: { revalidate: 10 },
   });
 
 export const getRange = () =>
   fetchFromAPI("/product-types", {
     // next: { revalidate: 600 },
+    next: { revalidate: 10 },
   });
 
 export const getFeaturedProducts = () =>
   fetchFromAPI("/products/featured", {
     // next: { revalidate: 600 },
+    next: { revalidate: 10 },
   });
 
 // ---------------- STATIC DATA APIs ----------------
 export const getLogoAndDesc = () =>
   fetchFromAPI("/theme-options/logo", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 export const getBannerAndDesc = () =>
   fetchFromAPI("/site-desc", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 export const getCertificates = () =>
   fetchFromAPI("/certifications", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 export const getSpeciality = () =>
   fetchFromAPI("/why-us", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 export const getTestimonials = () =>
   fetchFromAPI("/testimonials", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 // ---------------- NAVIGATION ----------------
 export const getNavItems = () =>
   fetchFromAPI("/menus", {
     // next: { revalidate: 600 },
+    next: { revalidate: 10 },
   });
 
 // --------------------------- BLOGS ---------------------------------
 export const getBlogs = () =>
   fetchFromAPI("/posts", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 export const getBlogCategories = () =>
   fetchFromAPI("/categories", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 export const getCategoryWiseBlogs = (slug: string) =>
   fetchFromAPI(`/categories/${slug}/posts`, {
     throw404: true,
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 export const getBlogDetail = (slug: string) =>
   fetchFromAPI(`/posts/${slug}`, {
     throw404: true,
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 // ---------------- PAGES ----------------
 export const getAboutUsData = () =>
   fetchFromAPI("/pages/about-us", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 export const getAboutUsInnerData = () =>
   fetchFromAPI("/aboutsection", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 export const getContactData = () =>
   fetchFromAPI("/contactad", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 export const getServicesData = () =>
   fetchFromAPI("/pages/services", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 export const getPcdOpportunity = () =>
   fetchFromAPI("/site-options/pcd-franchise", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 export const getCareerData = () =>
   fetchFromAPI("/pages/career", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
-  export const getTermsConditionsData = () =>
+export const getTermsConditionsData = () =>
   fetchFromAPI("/pages/terms-and-conditions", {
     // next: { revalidate: 3600 },
+    next: { revalidate: 10 },
   });
 
 // ---------------- TYPE/ CATEGORY/ PRODUCTS ROUTE API ----------------
@@ -155,17 +175,20 @@ export const getCategoryWiseProducts = (slug: string) =>
   fetchFromAPI(`/products/${slug}`, {
     throw404: true,
     // next: { revalidate: 600 },
+    next: { revalidate: 10 },
   });
 
 export const getTypeWiseProducts = (slug: string) =>
   fetchFromAPI(`/products/type/${slug}`, {
     throw404: true,
     // next: { revalidate: 600 },
+    next: { revalidate: 10 },
   });
 
 export const getAllProducts = () =>
   fetchFromAPI("/products", {
     // next: { revalidate: 600 },
+    next: { revalidate: 10 },
   });
 
 //-----------------------------------EXTRA CODE------------------------------------
